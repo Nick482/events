6689b128aca1cfe6a97a55b216e6fd90aeaa560d
Nick482 <kolya.bandura@gmail.com>
2016-09-22 10:50:20 +0300

    db

--- helpers/dbConnection.js
+++ helpers/dbConnection.js
@@ -1,3 +1,5 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/events');
+mongoose.connect('mongodb://admin:pass@ds033956.mlab.com:33956/events_db');
+
+//'mongodb://localhost:27017/events'
